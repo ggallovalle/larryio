32627f6116a884a3e2ffd85e8c6b1085a3e12e52
Gerson Gallo <gerson.gallo@leading2lean.com>
2024-11-27 15:10:26 -0600

    feat(web): POST /contacts

--- phone_books_web/__main__.py
+++ phone_books_web/__main__.py
@@ -2,9 +2,23 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
+import phone_books.contacts as core
+
 
 async def homepage(request):
     return JSONResponse({"hello": "world 2"})
+
+async def contacts_store(request):
+    data = await request.json()
+    assert isinstance(data, dict)
+    assert "name" in data
+    assert "phone" in data
+    assert "email" in data
+
+    contact = await core.create_contact(data)
+
+    return JSONResponse(contact)
+
 
 
 def make_app() -> Starlette:
@@ -12,6 +26,7 @@ def make_app() -> Starlette:
         debug=True,
         routes=[
             Route("/", homepage),
+            Route("/contacts", contacts_store, methods=["POST"]),
         ],
     )
     return app
